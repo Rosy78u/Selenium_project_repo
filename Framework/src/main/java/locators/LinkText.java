79c66500c8e9b968525904d81ad2451dd193add2
Jasmin Sahoo <156593274+Rosy78u@users.noreply.github.com>
2024-11-15 08:48:58 +0530

    Update LinkText.java

--- Framework/src/main/java/locators/LinkText.java
+++ Framework/src/main/java/locators/LinkText.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkText {
 	public static void main(String[] args) {
+		//modified to github
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
